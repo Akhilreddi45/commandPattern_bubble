4355c714146c28e9a0974a2e8071f8ac7d1a646d
baddamreddi45 <89807400+baddamreddi45@users.noreply.github.com>
2021-09-16 22:51:07 +0530

    machine

--- CommandPattern/src/com/example/command/bubble/SmallBubbleCircleMachine.java
+++ CommandPattern/src/com/example/command/bubble/SmallBubbleCircleMachine.java
@@ -1,20 +1,28 @@
 package com.example.command.bubble;
-
+/**
+* Small Bubble Circle Machine
+*/
 public class SmallBubbleCircleMachine {
 
 	public SmallBubbleCircleMachine() {
 
 	}
-
+	/**
+	* start machine.
+	*/
 	public void start() {
 		System.out.println("Small Bubble Circle Machine Started");
 	}
-
+	/**
+	* blow machine.
+	*/
 	public void blow() {
 		System.out.println("Small Bubble Circle Machine Blowing Circles");
 
-	}
-
+	}	
+	/**
+	* stop machine.
+	*/
 	public void stop() {
 		System.out.println("Small Bubble Circle Machine Stopped");
 
